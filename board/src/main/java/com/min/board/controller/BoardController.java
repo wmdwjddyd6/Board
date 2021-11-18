75db12dc31c72a8defe1efd3847f276e24695c65
BlueEar <67060302+wmdwjddyd6@users.noreply.github.com>
2021-11-18 22:44:30 +0900

    게시판 페이지 매핑연결
    form으로 데이터 저장하기

--- board/src/main/java/com/min/board/controller/BoardController.java
+++ board/src/main/java/com/min/board/controller/BoardController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -26,6 +28,14 @@ public class BoardController {
 
     @GetMapping("/form")
     public String form(Model model) {
+        model.addAttribute("board", new Board());
         return "board/form";
+    }
+
+    @PostMapping("/form")
+    public String boardSubmit(@ModelAttribute Board board) {
+        boardRepository.save(board);
+
+        return "redirect:/board/list";
     }
 }
