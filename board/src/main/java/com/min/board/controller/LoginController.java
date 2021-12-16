a5ff00fb4c4893f0d2ad49ac79b45fd07ed2a91a
BlueEar <67060302+wmdwjddyd6@users.noreply.github.com>
2021-12-16 14:04:55 +0900

    시큐리티 수정
    시큐리티 수정

--- board/src/main/java/com/min/board/controller/LoginController.java
+++ board/src/main/java/com/min/board/controller/LoginController.java
@@ -3,19 +3,20 @@ package com.min.board.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.validation.Valid;
 
 @Controller
 public class LoginController {
 
-    @GetMapping("login")
+    @GetMapping("/login")
     public String loginForm() {
         return "/account/loginForm";
     }
 
-    @PostMapping
-    public String login() {
-        // 로그인 처리
-    }
+//    @PostMapping
+//    public String login() {
+//        // 로그인 처리
+//    }
 }
