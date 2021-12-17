8f8d75c332edc6ccbccfe6191ede47907f3c9dff
BlueEar <67060302+wmdwjddyd6@users.noreply.github.com>
2021-12-17 17:18:24 +0900

    로그인 처리 완료
    로그인 처리 완료

---
+++ board/src/main/java/com/min/board/config/auth/PrincipalDetails.java
@@ -0,0 +1,72 @@
+package com.min.board.config.auth;
+
+import com.min.board.model.Member;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+/*
+    시큐리티가 로그인을 진행시키면 실행
+    로그인 진행이 완료되면 시큐리티 session을 생성 (Security ContextHolder라는 키 값으로 세션 정보 저장)
+    키에 대한 값으로 들어가는 오브젝트 타입 : Authentication
+    Authentication 안에 User(UserDetails타입) 정보가 있어야 됨.
+
+    요약 : Security Session 안에 => Authentication객체 안에 => UserDetails(PrincipalDetails)
+ */
+
+public class PrincipalDetails implements UserDetails {
+
+    private Member member; // 콤포지션
+
+    public PrincipalDetails(Member member) {
+        this.member = member;
+    }
+
+    // 해당 유저의 권한을 리턴
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> collection = new ArrayList<>();
+        collection.add(new GrantedAuthority() {
+            @Override
+            public String getAuthority() {
+                return member.getRole();
+            }
+        });
+        return collection;
+    }
+
+    @Override
+    public String getPassword() {
+        return member.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return member.getUsername();
+    }
+
+    // 계정이 일정 기간 만료는지 체크
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    // 비밀번호 너무 오래 사용했는지 체크
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    // 계정이 활성화 됐는지 체크 (~년 동안 로그인 안하면 휴면계정 등)
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
---
+++ board/src/main/java/com/min/board/config/auth/PrincipalDetailsService.java
@@ -0,0 +1,28 @@
+package com.min.board.config.auth;
+
+import com.min.board.model.Member;
+import com.min.board.repository.JpaMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+// /login 요청이 오면 자동으로 UserDetailsService 타입으로 IoC되어 있는 loadUserByUsername 함수가 실행
+@Service
+public class PrincipalDetailsService implements UserDetailsService {
+
+    @Autowired
+    private JpaMemberRepository jpaMemberRepository;
+
+    // 리턴되는 순간 시큐리티 session(내부 Authentication(내부 UserDetails))
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Member memberEntity = jpaMemberRepository.findByUsername(username);
+
+        if(memberEntity != null) {
+            return new PrincipalDetails(memberEntity);
+        }
+        return null;
+    }
+}
