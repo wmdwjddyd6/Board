c6b61341f5c4bd8c058cfdb4e3924a22b5907acf
BlueEar <67060302+wmdwjddyd6@users.noreply.github.com>
2021-12-17 17:16:43 +0900

    컨트롤러 매핑 위치 수정

--- board/src/main/java/com/min/board/controller/HomeController.java
+++ board/src/main/java/com/min/board/controller/HomeController.java
@@ -10,4 +10,9 @@ public class HomeController {
     public String Home(){
         return "index";
     }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "/admin";
+    }
 }
--- board/src/main/java/com/min/board/controller/LoginController.java
+++ board/src/main/java/com/min/board/controller/LoginController.java
@@ -2,10 +2,6 @@ package com.min.board.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import javax.validation.Valid;
 
 @Controller
 public class LoginController {
@@ -13,15 +9,5 @@ public class LoginController {
     @GetMapping("/loginForm")
     public String loginForm() {
         return "/account/loginForm";
-    }
-
-    @PostMapping("/login")
-    public void login() {
-        int a;
-    }
-
-    @GetMapping("/admin")
-    public String admin() {
-        return "/admin";
     }
 }
