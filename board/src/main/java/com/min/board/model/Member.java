89d9c07d3d1528ffedc7fd760f6aa0df21b55114
BlueEar <67060302+wmdwjddyd6@users.noreply.github.com>
2021-12-17 11:38:50 +0900

    Update Member.java
    Member 클래스 수정

--- board/src/main/java/com/min/board/model/Member.java
+++ board/src/main/java/com/min/board/model/Member.java
@@ -1,9 +1,16 @@
 package com.min.board.model;
 
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
 import javax.persistence.*;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.sql.Timestamp;
 
+@DynamicUpdate
+@DynamicInsert
 @Entity
 @Table(name = "tb_userinfo")
 public class Member {
@@ -21,7 +28,15 @@ public class Member {
     private String password;
 
     @NotNull
+    @Size(min = 6, max = 45, message = "이메일은 6자 이상 45자 이하 입니다.")
+    @Email
+    private String email;
+
+    @Column
     private String role;
+
+//    @CreationTimestamp
+//    private Timestamp createDate;
 
     public long getId() {
         return id;
@@ -47,6 +62,14 @@ public class Member {
         this.password = password;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public String getRole() {
         return role;
     }
@@ -54,4 +77,12 @@ public class Member {
     public void setRole(String role) {
         this.role = role;
     }
+
+//    public Timestamp getCreateDate() {
+//        return createDate;
+//    }
+//
+//    public void setCreateDate(Timestamp createDate) {
+//        this.createDate = createDate;
+//    }
 }
