f4145c1250592543cd0339ef4c3dcd53f3a5988c
wmdwjddyd6 <wmdwjddyd6@naver.com>
2021-12-20 20:13:03 +0900

    사용자 메뉴 이동 링크 추가

---
+++ board/src/main/java/com/min/board/controller/AccountController.java
@@ -0,0 +1,51 @@
+package com.min.board.controller;
+
+import com.min.board.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
+
+@Controller
+@RequestMapping("/account")
+public class AccountController {
+
+    @Autowired
+    private MemberService memberService;
+
+    // 유저 메뉴 탭으로 이동
+    @GetMapping("/userMenu")
+    public String userMenu(Model model, Principal principal) {
+        model.addAttribute("userEmail", memberService.getUserEmail(principal.getName()));
+
+        return "/account/userMenu";
+    }
+
+    // 회원탈퇴 화면으로 이동
+    @GetMapping("/secessionForm")
+    public String secessionForm() {
+        return "/account/secessionForm";
+    }
+
+    // 회원탈퇴 진행
+    @PostMapping("/secession")
+    public String secession(Principal principal, String password) {
+       String username = principal.getName();
+
+       if(memberService.checkPassword(username, password)) {
+           memberService.secession(username);
+           SecurityContextHolder.clearContext(); // 회원탈퇴 시 로그아웃
+
+           return "redirect:/";
+       } else {
+           System.out.println("진행실패");
+           return "redirect:/account/secessionForm?error=true";
+       }
+    }
+
+}
