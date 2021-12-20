b774b046672eae5d8340bd3fed420450a89514ef
wmdwjddyd6 <wmdwjddyd6@naver.com>
2021-12-20 21:24:22 +0900

    회원탈퇴 관련 서비스 추가

--- board/src/main/java/com/min/board/service/MemberService.java
+++ board/src/main/java/com/min/board/service/MemberService.java
@@ -1,6 +1,7 @@
 package com.min.board.service;
 
 import com.min.board.model.Member;
+import com.min.board.model.MemberID;
 import com.min.board.repository.JpaMemberRepository;
 import com.min.board.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,10 +28,20 @@ public class MemberService {
         this.jpaMemberRepository = jpaMemberRepository;
     }
 
+    // 회원가입
     public void join(Member member){
         jpaMemberRepository.save(member);
     }
 
+    // 회원탈퇴
+    public void secession(String username) {
+        Member member = jpaMemberRepository.findByUsername(username);
+        MemberID memberID = new MemberID(member.getId(), member.getUsername()); // 기본키 셋팅
+
+        jpaMemberRepository.deleteById(memberID);
+    }
+
+    // 패스워드 암호화
     public String pwdEncoding(String password) {
         return bCryptPasswordEncoder.encode(password);
     }
@@ -39,7 +50,21 @@ public class MemberService {
         return jpaMemberRepository.findByUsername(username).getId();
     }
 
+    public String getUserEmail(String username) {
+        return jpaMemberRepository.findByUsername(username).getEmail();
+    }
+
     public boolean checkUsername(String username) {
         return jpaMemberRepository.existsByUsername(username);
+    }
+
+    public boolean checkPassword(String username, String password) {
+        Member member = jpaMemberRepository.findByUsername(username);
+
+        if(bCryptPasswordEncoder.matches(password, member.getPassword())) {
+            return true;
+        }
+
+        return false;
     }
 }
