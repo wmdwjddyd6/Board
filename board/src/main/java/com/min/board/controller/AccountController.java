98332fef9a21604d3e2f09b841f0bd70bd27a5e4
wmdwjddyd6 <wmdwjddyd6@naver.com>
2021-12-20 23:49:07 +0900

    비밀번호 변경 컨트롤러 추가

--- board/src/main/java/com/min/board/controller/AccountController.java
+++ board/src/main/java/com/min/board/controller/AccountController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.security.Principal;
 
+// 유저 메뉴 항목 관련 컨트롤러
 @Controller
 @RequestMapping("/account")
 public class AccountController {
@@ -35,16 +36,40 @@ public class AccountController {
 
     // 회원탈퇴 진행
     @PostMapping("/secession")
-    public String secession(Principal principal, String password, @RequestBody(required = false) String check) {
-        String username = principal.getName();
+    public String secession(Principal principal, String password,
+                            @RequestBody(required = false) String check) {
+        String loginUsername = principal.getName();
 
-        if(memberService.checkPassword(username, password) && !check.isEmpty()) {
-           memberService.secession(username);
+        if(memberService.checkPassword(loginUsername, password) && !check.isEmpty()) { // 패스워드, 체크박스 확인
+           memberService.secession(loginUsername); // 회원탈퇴
            SecurityContextHolder.clearContext(); // 회원탈퇴 시 로그아웃
 
            return "redirect:/";
         } else {
            return "redirect:/account/secessionForm?error=true";
+        }
+    }
+
+    // 비밀번호 변경 화면으로 이동
+    @GetMapping("/changePasswordForm")
+    public String changePasswordForm() {
+        return "/account/changePasswordForm";
+    }
+
+    // 비밀번호 변경
+    @PostMapping("/changePassword")
+    public String changePassword(String oldPassword, String newPassword, Principal principal) {
+        String loginUsername = principal.getName();
+
+        if(oldPassword.isEmpty() || newPassword.isEmpty()) {
+            return "redirect:/account/changePasswordForm?error=true";
+        } else if(memberService.checkPassword(loginUsername, oldPassword)) { // 이전 비밀번호 체크
+            memberService.changePassword(loginUsername, newPassword); // 비밀번호 변경
+            SecurityContextHolder.clearContext();
+
+            return "redirect:/";
+        } else {
+            return "redirect:/account/changePasswordForm?error=true";
         }
     }
 }
