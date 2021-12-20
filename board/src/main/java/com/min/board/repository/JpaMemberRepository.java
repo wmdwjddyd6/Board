5dc2df2c7b60c90dea01aa296ccc353aec43afd9
wmdwjddyd6 <wmdwjddyd6@naver.com>
2021-12-20 21:25:34 +0900

    JPA 기본키 변경 적용

--- board/src/main/java/com/min/board/repository/JpaMemberRepository.java
+++ board/src/main/java/com/min/board/repository/JpaMemberRepository.java
@@ -1,9 +1,10 @@
 package com.min.board.repository;
 
 import com.min.board.model.Member;
+import com.min.board.model.MemberID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface JpaMemberRepository extends JpaRepository<Member, Long> {
+public interface JpaMemberRepository extends JpaRepository<Member, MemberID> {
 
     public Member findByUsername(String username);
 
