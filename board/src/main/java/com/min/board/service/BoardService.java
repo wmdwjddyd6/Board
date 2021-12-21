4e0bb3ad4993066d22ccb966dc906ea48ac053ba
wmdwjddyd6 <wmdwjddyd6@naver.com>
2021-12-22 00:35:17 +0900

    BoardService DB 변경

--- board/src/main/java/com/min/board/service/BoardService.java
+++ board/src/main/java/com/min/board/service/BoardService.java
@@ -18,41 +18,40 @@ import java.util.Optional;
 @Service
 public class BoardService {
 
-    private final BoardMapper boardMapper;
+    private final BoardMapper boardRepository;
 
     @Autowired
     public BoardService(BoardMapper boardMapper) {
-        this.boardMapper = boardMapper;
+        this.boardRepository = boardMapper;
     }
 
 //    // 검색 : 제목과 내용으로 검색
 //    public Page<Board> search(String title, String content, Pageable pageable) {
-//        Page<Board> boards = boardMapper.findByTitleContainingOrContentContaining(title, content, pageable);
+//        Page<Board> boards = boardRepository.findByTitleContainingOrContentContaining(title, content, pageable);
 //        return boards;
 //    }
 
-    // 내가 쓴 글 리스트 뽑기
-    public Page<Board> possessionContentLoad(Pageable pageable, Member member) {
-        EntityManager entityManager = null;
-        Page<Board> boards = (Page) entityManager.find(Board.class, member);
-        return boards;
-    }
+//    // 내가 쓴 글 리스트 뽑기
+//    public Page<Board> possessionContentLoad(Pageable pageable, Member member) {
+//        EntityManager entityManager = null;
+//        Page<Board> boards = (Page) entityManager.find(Board.class, member);
+//        return boards;
+//    }
 
     // id를 이용해서 해당 글 수정
     public Board contentLoad(Long id) {
-        Board board = boardMapper.findById(id);
+        Board board = boardRepository.findById(id);
         return board;
     }
 
     // 글 등록
     public void save(Board board) {
-
-        boardMapper.insertBoard(board); // 이게 문제네
+        boardRepository.insertBoard(board); // 이게 문제네
         System.out.println("여기2");
     }
 
-    // 글 삭제
+    // 글 임시 삭제 (업데이트 로직 실행)
     public void temporaryDelete(Long boardId) {
-        boardMapper.temporaryDeleteById(boardId);
+        boardRepository.temporaryDeleteById(boardId);
     }
 }
