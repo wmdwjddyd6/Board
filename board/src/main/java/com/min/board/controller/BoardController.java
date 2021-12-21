9d84999e574be17ea1a40f6798d4801e7b83e774
wmdwjddyd6 <wmdwjddyd6@naver.com>
2021-12-21 20:23:36 +0900

    MyBatis 이후 Board 소스 수정

--- board/src/main/java/com/min/board/controller/BoardController.java
+++ board/src/main/java/com/min/board/controller/BoardController.java
@@ -1,8 +1,10 @@
 package com.min.board.controller;
 
 import com.min.board.model.Board;
+import com.min.board.model.Member;
 import com.min.board.service.BoardService;
 import com.min.board.service.MemberService;
+import org.dom4j.rule.Mode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -26,33 +28,33 @@ public class BoardController {
     @Autowired
     private MemberService memberService;
 
-    // 게시판 리스트 페이징 및 검색 리스트
-    @GetMapping("/list")
-    public String list(Model model, @PageableDefault(size = 2) Pageable pageable,
-                       @RequestParam(required = false, defaultValue = "") String searchText) {
-        Page<Board> boards = boardService.search(searchText, searchText, pageable);
-        int startPage = Math.max(1, boards.getPageable().getPageNumber() - 4);
-        int endPage = Math.min(boards.getTotalPages(), boards.getPageable().getPageNumber() + 4);
+//    // 게시판 리스트 페이징 및 검색 리스트
+//    @GetMapping("/list")
+//    public String list(Model model, @PageableDefault(size = 2) Pageable pageable,
+//                       @RequestParam(required = false, defaultValue = "") String searchText) {
+//        Page<Board> boards = boardService.search(searchText, searchText, pageable);
+//        int startPage = Math.max(1, boards.getPageable().getPageNumber() - 4);
+//        int endPage = Math.min(boards.getTotalPages(), boards.getPageable().getPageNumber() + 4);
+//
+//        model.addAttribute("startPage", startPage);
+//        model.addAttribute("endPage", endPage);
+//        model.addAttribute("boards", boards);
+//
+//        return "board/list";
+//    }
 
-        model.addAttribute("startPage", startPage);
-        model.addAttribute("endPage", endPage);
-        model.addAttribute("boards", boards);
-
-        return "board/list";
-    }
-
-    // 게시글 신규 작성 폼 진입 & 기존 게시글 불러오기
-    @GetMapping("/form")
-    public String form(Model model, @RequestParam(required = false) Long boardId) {
-        if(boardId == null) {
-            model.addAttribute("board", new Board());
-        } else {
-            Board board = boardService.contentLoad(boardId);
-            model.addAttribute("board", board);
-        }
-
-        return "board/form";
-    }
+//    // 게시글 신규 작성 폼 진입 & 기존 게시글 불러오기
+//    @GetMapping("/form")
+//    public String form(Model model, @RequestParam(required = false) Long boardId) {
+//        if(boardId == null) {
+//            model.addAttribute("board", new Board());
+//        } else {
+//            Board board = boardService.contentLoad(boardId);
+//            model.addAttribute("board", board);
+//        }
+//
+//        return "board/form";
+//    }
 
     // 게시글 작성 & 수정
     @PostMapping("/form")
@@ -61,9 +63,9 @@ public class BoardController {
             return "board/form";
         }
         String loginUsername = principal.getName();
+        Long id = memberService.getUserId(loginUsername);
 
-        board.getMember().setId(memberService.getUserId(loginUsername));
-        board.getMember().setUsername(loginUsername);
+        board.setWriterId(id);
 
         boardService.save(board);
 
@@ -81,8 +83,30 @@ public class BoardController {
     // 게시글 삭제
     @PostMapping("/delete")
     public String boardDelete(Long boardId) {
-        boardService.delete(boardId);
+        boardService.temporaryDelete(boardId);
 
         return "redirect:/board/list";
+    }
+
+    // 내가 쓴 글 화면 이동
+    @GetMapping("/myPost")
+    public String myPost(Model model, Principal principal, @PageableDefault(size = 5) Pageable pageable) {
+        Member member = memberService.getMember(principal.getName());
+        Page<Board> boards = boardService.possessionContentLoad(pageable, member);
+
+        int startPage = Math.max(1, boards.getPageable().getPageNumber() - 4);
+        int endPage = Math.min(boards.getTotalPages(), boards.getPageable().getPageNumber() + 4);
+
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
+        model.addAttribute("boards", boards);
+
+        return "/board/myPost";
+    }
+
+    // 휴지통 화면 이동
+    @GetMapping("/trash")
+    public String trashList() {
+        return "/board/trash";
     }
 }
--- board/src/main/java/com/min/board/service/BoardService.java
+++ board/src/main/java/com/min/board/service/BoardService.java
@@ -1,41 +1,58 @@
 package com.min.board.service;
 
 import com.min.board.model.Board;
+import com.min.board.model.Member;
+import com.min.board.repository.BoardMapper;
 import com.min.board.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityManager;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class BoardService {
 
-    private final BoardRepository boardRepository;
+    private final BoardMapper boardMapper;
 
     @Autowired
-    public BoardService(BoardRepository boardRepository) {
-        this.boardRepository = boardRepository;
+    public BoardService(BoardMapper boardMapper) {
+        this.boardMapper = boardMapper;
     }
 
-    // 검색 : 제목과 내용으로 검색
-    public Page<Board> search(String title, String content, Pageable pageable) {
-        Page<Board> boards = boardRepository.findByTitleContainingOrContentContaining(title, content, pageable);
+//    // 검색 : 제목과 내용으로 검색
+//    public Page<Board> search(String title, String content, Pageable pageable) {
+//        Page<Board> boards = boardMapper.findByTitleContainingOrContentContaining(title, content, pageable);
+//        return boards;
+//    }
+
+    // 내가 쓴 글 리스트 뽑기
+    public Page<Board> possessionContentLoad(Pageable pageable, Member member) {
+        EntityManager entityManager = null;
+        Page<Board> boards = (Page) entityManager.find(Board.class, member);
         return boards;
     }
 
     // id를 이용해서 해당 글 수정
     public Board contentLoad(Long id) {
-        Board board = boardRepository.findById(id).orElse(null);
+        Board board = boardMapper.findById(id);
         return board;
     }
 
     // 글 등록
     public void save(Board board) {
-        boardRepository.save(board);
+
+        boardMapper.insertBoard(board); // 이게 문제네
+        System.out.println("여기2");
     }
 
     // 글 삭제
-    public void delete(Long boardId) {
-        boardRepository.deleteById(boardId);
+    public void temporaryDelete(Long boardId) {
+        boardMapper.temporaryDeleteById(boardId);
     }
 }
