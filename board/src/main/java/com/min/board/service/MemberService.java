77fff0eb3e15a77c7af52c7b8908d2adb54c0b11
wmdwjddyd6 <wmdwjddyd6@naver.com>
2021-12-22 00:35:03 +0900

    MemberService DB 변경

--- board/src/main/java/com/min/board/service/MemberService.java
+++ board/src/main/java/com/min/board/service/MemberService.java
@@ -1,8 +1,9 @@
 package com.min.board.service;
 
 import com.min.board.model.Member;
-import com.min.board.model.MemberID;
+//import com.min.board.model.MemberID;
 import com.min.board.repository.JpaMemberRepository;
+import com.min.board.repository.MemberMapper;
 import com.min.board.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -20,49 +21,31 @@ public class MemberService {
 //        this.memberRepository = memberRepository;
 //    }
 
-    private final JpaMemberRepository jpaMemberRepository;
+    private final MemberMapper memberRepository;
 
     @Autowired
     private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Autowired
-    public MemberService(JpaMemberRepository jpaMemberRepository) {
-        this.jpaMemberRepository = jpaMemberRepository;
+    public MemberService(MemberMapper memberMapper) {
+        this.memberRepository = memberMapper;
     }
 
     // 회원가입
     public void join(Member member){
-        jpaMemberRepository.save(member);
+        memberRepository.save(member);
     }
 
     // 회원탈퇴
     public void secession(String username) {
-        Member member = jpaMemberRepository.findByUsername(username);
-        MemberID memberID = new MemberID(member.getId(), member.getUsername()); // 기본키 셋팅
+        Member member = memberRepository.findByUsername(username);
 
-        jpaMemberRepository.deleteById(memberID);
-    }
-
-    // 패스워드 암호화
-    public String pwdEncoding(String password) {
-        return bCryptPasswordEncoder.encode(password);
-    }
-
-    public Long getUserId(String username) {
-        return jpaMemberRepository.findByUsername(username).getId();
-    }
-
-    public String getUserEmail(String username) {
-        return jpaMemberRepository.findByUsername(username).getEmail();
-    }
-
-    public boolean checkUsername(String username) {
-        return jpaMemberRepository.existsByUsername(username);
+        memberRepository.delete(member);
     }
 
     // 비밀번호 체크
     public boolean checkPassword(String loginUsername, String password) {
-        Member member = jpaMemberRepository.findByUsername(loginUsername);
+        Member member = memberRepository.findByUsername(loginUsername);
 
         if(bCryptPasswordEncoder.matches(password, member.getPassword())) {
             return true;
@@ -73,9 +56,39 @@ public class MemberService {
     // 비밀번호 변경
     public void changePassword(String loginUsername, String newPassword) {
         String encPassword = pwdEncoding(newPassword);
-        Member member = jpaMemberRepository.findByUsername(loginUsername);
+        Member member = memberRepository.findByUsername(loginUsername);
         member.setPassword(encPassword);
 
-        jpaMemberRepository.save(member);
+        memberRepository.pwdChange(member);
+    }
+
+    // 패스워드 암호화
+    public String pwdEncoding(String password) {
+        return bCryptPasswordEncoder.encode(password);
+    }
+
+    public Member getMember(String username) {
+        return memberRepository.findByUsername(username);
+    }
+
+    public Long getUserId(String username) {
+        return memberRepository.findByUsername(username).getId();
+    }
+
+    public String getUserEmail(String username) {
+        return memberRepository.findByUsername(username).getEmail();
+    }
+
+    // 아이디 중복 체크
+    public boolean checkUsername(String username) {
+        try {
+            if(memberRepository.findByUsername(username).getUsername().equals(username)) {
+                return false;
+            } else {
+                return true;
+            }
+        } catch (Exception e) {
+            return false;
+        }
     }
 }
