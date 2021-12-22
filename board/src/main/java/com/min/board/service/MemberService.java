b353f773ef2390edbec1e6759c8dda3b6a5fc100
wmdwjddyd6 <wmdwjddyd6@naver.com>
2021-12-22 13:18:44 +0900

    MemberService 예외 처리 추가

--- board/src/main/java/com/min/board/service/MemberService.java
+++ board/src/main/java/com/min/board/service/MemberService.java
@@ -26,6 +26,7 @@ public class MemberService {
     @Autowired
     private BCryptPasswordEncoder bCryptPasswordEncoder;
 
+
     @Autowired
     public MemberService(MemberMapper memberMapper) {
         this.memberRepository = memberMapper;
@@ -33,33 +34,55 @@ public class MemberService {
 
     // 회원가입
     public void join(Member member){
-        memberRepository.save(member);
+        if(checkUsername(member.getUsername())) { // 아이디 중복 2차 체크 (1차는 컨트롤러의 Validator에서 진행)
+            System.out.println("memberService.join() : 이미 존재하는 아이디입니다.");
+        } else {
+            try {
+                memberRepository.save(member);
+                System.out.println("memberService.join() : 회원가입 완료");
+            } catch (Exception e) {
+                System.out.println("memberService.join() .. error : " + e.getMessage());
+            }
+        }
     }
 
     // 회원탈퇴
     public void secession(String username) {
-        Member member = memberRepository.findByUsername(username);
+        Member member = getMember(username);
 
-        memberRepository.delete(member);
+        try{
+            memberRepository.delete(member);
+            System.out.println("memberService.secession() : 회원탈퇴 완료");
+        } catch (Exception e) {
+            System.out.println("memberService.secession() .. error : " + e.getMessage());
+        }
     }
 
     // 비밀번호 체크
     public boolean checkPassword(String loginUsername, String password) {
-        Member member = memberRepository.findByUsername(loginUsername);
+        Member member = getMember(loginUsername);
 
         if(bCryptPasswordEncoder.matches(password, member.getPassword())) {
+            System.out.println("memberService.checkPassword() : 암호가 일치합니다.");
             return true;
+        } else {
+            System.out.println("memberService.checkPassword() : 암호가 일치하지 않습니다.");
+            return false;
         }
-        return false;
     }
 
     // 비밀번호 변경
     public void changePassword(String loginUsername, String newPassword) {
         String encPassword = pwdEncoding(newPassword);
-        Member member = memberRepository.findByUsername(loginUsername);
+        Member member = getMember(loginUsername);
         member.setPassword(encPassword);
 
-        memberRepository.pwdChange(member);
+        try{
+            memberRepository.pwdChange(member);
+            System.out.println("memberService.changePassword() : Password 변경 완료");
+        } catch (Exception e) {
+            System.out.println("memberService.changePassword() .. error : " + e.getMessage());
+        }
     }
 
     // 패스워드 암호화
@@ -68,26 +91,31 @@ public class MemberService {
     }
 
     public Member getMember(String username) {
-        return memberRepository.findByUsername(username);
-    }
+        Member member = new Member();
 
-    public Long getUserId(String username) {
-        return memberRepository.findByUsername(username).getId();
-    }
-
-    public String getUserEmail(String username) {
-        return memberRepository.findByUsername(username).getEmail();
+        try{
+            member = memberRepository.findByUsername(username);
+            System.out.println("memberService.getMember() : 멤버 객체 초기화 완료");
+            return member;
+        } catch (Exception e) {
+            System.out.println("memberService.getMember() .. error : " + e.getMessage());
+            return member;
+        }
     }
 
     // 아이디 중복 체크
     public boolean checkUsername(String username) {
+        // true 반환 시 중복 아이디 있음
+        Member member = getMember(username);
+
         try {
-            if(memberRepository.findByUsername(username).getUsername().equals(username)) {
-                return false;
-            } else {
+            if(member.getUsername().equals(username)) {
                 return true;
+            } else {
+                return false;
             }
         } catch (Exception e) {
+            System.out.println("error : " + e.getMessage()); // DB 조회 결과가 없을 때 error : null 출력
             return false;
         }
     }
