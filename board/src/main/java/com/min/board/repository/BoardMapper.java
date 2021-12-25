a757c697197fd8f6a8073735c853417f3f1c7c64
wmdwjddyd6 <wmdwjddyd6@naver.com>
2021-12-25 17:29:18 +0900

    게시판 페이징 관련 업데이트

--- board/src/main/java/com/min/board/repository/BoardMapper.java
+++ board/src/main/java/com/min/board/repository/BoardMapper.java
@@ -1,6 +1,7 @@
 package com.min.board.repository;
 
 import com.min.board.model.Board;
+import com.min.board.paging.Pagination;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -8,10 +9,23 @@ import java.util.Optional;
 
 @Mapper
 public interface BoardMapper {
+    // 모든 글 조회
     List<Board> selectAllBoards();
+
+    // 삭제된 글 제외 모두 조회
+    List<Board> selectBoardList(Pagination pagination);
+
+    int selectBoardTotalCount();
+
+    // 게시글 수정
     void updateBoard(Board board);
 
+    // 아이디로 글 찾기
     Board findById(Long id);
+
+    // 휴지통으로 이동 (임시삭제)
     void temporaryDeleteById(Long id);
+
+    // 글 작성
     void insertBoard(Board board);
 }
