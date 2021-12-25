006beacede4ed42a570c50b4ee3c015f819e5730
wmdwjddyd6 <wmdwjddyd6@naver.com>
2021-12-25 17:30:05 +0900

    게시판 페이징 컨트롤러 및 html 추가

--- board/src/main/java/com/min/board/controller/BoardController.java
+++ board/src/main/java/com/min/board/controller/BoardController.java
@@ -2,6 +2,7 @@ package com.min.board.controller;
 
 import com.min.board.model.Board;
 import com.min.board.model.Member;
+import com.min.board.paging.Pagination;
 import com.min.board.service.BoardService;
 import com.min.board.service.MemberService;
 import org.dom4j.rule.Mode;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.security.Principal;
+import java.util.List;
 
 @Controller
 @RequestMapping("/board")
@@ -28,20 +30,22 @@ public class BoardController {
     @Autowired
     private MemberService memberService;
 
-//    // 게시판 리스트 페이징 및 검색 리스트
-//    @GetMapping("/list")
-//    public String list(Model model, @PageableDefault(size = 2) Pageable pageable,
-//                       @RequestParam(required = false, defaultValue = "") String searchText) {
-//        Page<Board> boards = boardService.search(searchText, searchText, pageable);
-//        int startPage = Math.max(1, boards.getPageable().getPageNumber() - 4);
-//        int endPage = Math.min(boards.getTotalPages(), boards.getPageable().getPageNumber() + 4);
-//
-//        model.addAttribute("startPage", startPage);
-//        model.addAttribute("endPage", endPage);
-//        model.addAttribute("boards", boards);
-//
-//        return "board/list";
-//    }
+    // 게시판 리스트      게시글 페이징 및 검색 리스트
+    @GetMapping("/list")
+    public String list(Model model,
+                       @RequestParam(required = false, defaultValue = "1") int page,
+                       @RequestParam(required = false, defaultValue = "1") int range,
+                       String searchText) {
+        int listCount = boardService.getBoardListCnt();
+
+        Pagination pagination = new Pagination();
+        pagination.pageInfo(page, range, listCount);
+
+        model.addAttribute("pagination", pagination);
+        model.addAttribute("boardList", boardService.getBoardList(pagination));
+
+        return "board/list";
+    }
 
 //    // 게시글 신규 작성 폼 진입 & 기존 게시글 불러오기
 //    @GetMapping("/form")
@@ -63,11 +67,12 @@ public class BoardController {
             return "board/form";
         }
         String loginUsername = principal.getName();
-        Long id = memberService.getUserId(loginUsername);
+        Member member = memberService.getMember(loginUsername);
+        Long id = member.getId();
 
         board.setWriterId(id);
 
-        boardService.save(board);
+        boardService.save(board); // 이 부분 업데이트랑 인서트 분리해야함 ~~~~~~~~~~~~~~~~~~~~~~~~~~~현재는 인서트임~~~~~~~~~
 
         return "redirect:/board/list";
     }
@@ -88,21 +93,21 @@ public class BoardController {
         return "redirect:/board/list";
     }
 
-    // 내가 쓴 글 화면 이동
-    @GetMapping("/myPost")
-    public String myPost(Model model, Principal principal, @PageableDefault(size = 5) Pageable pageable) {
-        Member member = memberService.getMember(principal.getName());
-        Page<Board> boards = boardService.possessionContentLoad(pageable, member);
-
-        int startPage = Math.max(1, boards.getPageable().getPageNumber() - 4);
-        int endPage = Math.min(boards.getTotalPages(), boards.getPageable().getPageNumber() + 4);
-
-        model.addAttribute("startPage", startPage);
-        model.addAttribute("endPage", endPage);
-        model.addAttribute("boards", boards);
-
-        return "/board/myPost";
-    }
+//    // 내가 쓴 글 화면 이동
+//    @GetMapping("/myPost")
+//    public String myPost(Model model, Principal principal, @PageableDefault(size = 5) Pageable pageable) {
+//        Member member = memberService.getMember(principal.getName());
+//        Page<Board> boards = boardService.possessionContentLoad(pageable, member);
+//
+//        int startPage = Math.max(1, boards.getPageable().getPageNumber() - 4);
+//        int endPage = Math.min(boards.getTotalPages(), boards.getPageable().getPageNumber() + 4);
+//
+//        model.addAttribute("startPage", startPage);
+//        model.addAttribute("endPage", endPage);
+//        model.addAttribute("boards", boards);
+//
+//        return "/board/myPost";
+//    }
 
     // 휴지통 화면 이동
     @GetMapping("/trash")
