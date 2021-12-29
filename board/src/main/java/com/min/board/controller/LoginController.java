53bdee4be6447e02f4f498ce7bcf6bb8b9fdc3f7
wmdwjddyd6 <wmdwjddyd6@naver.com>
2021-12-29 14:31:00 +0900

    ID 찾기 GET, POST 컨트롤러 추가

--- board/src/main/java/com/min/board/controller/LoginController.java
+++ board/src/main/java/com/min/board/controller/LoginController.java
@@ -1,14 +1,23 @@
 package com.min.board.controller;
 
 import com.min.board.model.Member;
+import com.min.board.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.validation.Valid;
+import java.util.List;
+
 @Controller
 public class LoginController {
+
+    @Autowired
+    private MemberService memberService;
 
     // 로그인 폼
     @GetMapping("/loginForm")
@@ -26,10 +35,26 @@ public class LoginController {
 
     // ID 찾기
     @GetMapping("findIdForm")
-    public String findIdForm(Model model) {
-        model.addAttribute("member", new Member());
+    public String findIdForm(@RequestParam(required = false) String email, Model model) {
+        if(email != null) {
+            List<Member> memberList = memberService.getMemberByEmail(email);
+            model.addAttribute("memberList", memberList);
+        }
         return "/account/findIdForm";
     }
+
+    // ID 찾기
+    @PostMapping("/findId")
+    public String findId(String email, Model model){ // view의 form->input 의 name과 매핑됨.
+        if(memberService.checkEmail(email)) {
+            String resultUsername = "";
+
+            return "redirect:/findIdForm?email=" + email;
+        } else {
+            return "redirect:/findIdForm?error=true";
+        }
+    }
+
 
     // PW 찾기
     @GetMapping("passwordReset")
