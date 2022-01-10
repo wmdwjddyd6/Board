d0751a04212b3dba00ea0c71ba225a1b52293fed
wmdwjddyd6 <wmdwjddyd6@naver.com>
2022-01-10 17:13:37 +0900

    이미지 Get Mapper 구현

--- board/src/main/java/com/min/board/repository/mapper/FileMapper.java
+++ board/src/main/java/com/min/board/repository/mapper/FileMapper.java
@@ -3,7 +3,14 @@ package com.min.board.repository.mapper;
 import com.min.board.model.FileDTO;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.sql.SQLException;
+import java.util.List;
+
 @Mapper
 public interface FileMapper {
-    int insertFile(FileDTO fileDTO);
+    // 파일 첨부
+    int insertFile(FileDTO fileDTO) throws SQLException;
+
+    // 파일 리스트 불러오기
+    List<FileDTO> selectByBoardId(Long boardId) throws SQLException;
 }
