f017bc1e2282afe6bc66cab096648c015945d7d8
wmdwjddyd6 <wmdwjddyd6@naver.com>
2022-01-13 14:10:46 +0900

    [AdminController] 회원삭제 추가

---
+++ board/src/main/java/com/min/board/controller/AdminController.java
@@ -0,0 +1,24 @@
+package com.min.board.controller;
+
+import com.min.board.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+public class AdminController {
+
+    @Autowired
+    private MemberService memberService;
+
+
+    @ResponseBody
+    @DeleteMapping("/members")
+    public void deleteMembers(@RequestParam(name = "memberIdList[]", required = false) List<String> memberIdList) {
+        memberService.deleteMember(memberIdList);
+    }
+}
