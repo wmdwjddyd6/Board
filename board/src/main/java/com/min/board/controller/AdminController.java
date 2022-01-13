fb011c8d79c7a3165f73d4ac51f5d6e2d63e9bff
wmdwjddyd6 <wmdwjddyd6@naver.com>
2022-01-13 21:19:53 +0900

    [Controller] 관리자 생성 추가

--- board/src/main/java/com/min/board/controller/AdminController.java
+++ board/src/main/java/com/min/board/controller/AdminController.java
@@ -1,12 +1,15 @@
 package com.min.board.controller;
 
+import com.min.board.model.Member;
 import com.min.board.service.MemberService;
+import com.min.board.validator.MemberValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -15,6 +18,32 @@ public class AdminController {
     @Autowired
     private MemberService memberService;
 
+    @Autowired
+    private MemberValidator memberValidator;
+
+    // 관리자 계정 생성 폼
+    @GetMapping("/admin/addAdmin")
+    public String addAdminForm(Model model) {
+        model.addAttribute("member", new Member());
+        return "/admin/addAdmin";
+    }
+
+    // 관리자 계정 생성
+    @PostMapping("/admin/join")
+    public String addMember(@Valid Member member, BindingResult bindingResult){ // view의 form->input 의 name과 매핑됨.
+        memberValidator.validate(member, bindingResult);
+
+        if(bindingResult.hasErrors()) {
+            return "/admin/addAdmin";
+        }
+
+        int result = memberService.join(member, "ROLE_ADMIN");
+        if(result > 0) {
+            return "redirect:/admin";
+        } else {
+            return "/admin/addAdmin";
+        }
+    }
 
     // 회원계정 삭제
     @ResponseBody
