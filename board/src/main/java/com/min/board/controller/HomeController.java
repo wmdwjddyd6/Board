a8a05c50ae998701a67d9482c8753899e36fbfa1
wmdwjddyd6 <wmdwjddyd6@naver.com>
2022-01-14 20:18:39 +0900

    [Controller, View] 특정 유저 게시글 개수 추가
    Controller - memberList -> memberMap으로 수정
    View - 타임리프 member데이터 map으로 수정

--- board/src/main/java/com/min/board/controller/HomeController.java
+++ board/src/main/java/com/min/board/controller/HomeController.java
@@ -1,7 +1,9 @@
 package com.min.board.controller;
 
+import com.min.board.model.Board;
 import com.min.board.model.Member;
 import com.min.board.paging.Pagination;
+import com.min.board.service.BoardService;
 import com.min.board.service.MemberService;
 import com.min.board.service.PagingService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,13 +12,18 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 public class HomeController {
 
     @Autowired
     private MemberService memberService;
+
+    @Autowired
+    private BoardService boardService;
 
     @Autowired
     private PagingService pagingService;
@@ -34,9 +41,16 @@ public class HomeController {
                             @RequestParam(required = false, defaultValue = "1") int range,
                             String searchText) {
         Pagination pagination = pagingService.getMemberPagination(page, range, searchText);
-        List<Member> memberList = memberService.getMemberList(pagination);
+        List<Member> members = memberService.getMemberList(pagination);
 
-        model.addAttribute("memberList", memberList);
+        Map<Member,Long> memberMap = new HashMap<>();
+
+        for(Member member : members) {
+            Long boardCount = boardService.getSpecificBoardCnt(member.getId());
+            memberMap.put(member, boardCount);
+        }
+
+        model.addAttribute("memberMap", memberMap);
         model.addAttribute("pagination", pagination);
 
         return "/admin/adminHome";
