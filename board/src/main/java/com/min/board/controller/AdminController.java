518651e51f6924e137dfd29ba76d87e1efcbd595
wmdwjddyd6 <wmdwjddyd6@naver.com>
2022-01-14 17:36:22 +0900

    [Controller] 게시글 관리 - 완료

--- board/src/main/java/com/min/board/controller/AdminController.java
+++ board/src/main/java/com/min/board/controller/AdminController.java
@@ -1,7 +1,11 @@
 package com.min.board.controller;
 
+import com.min.board.model.Board;
 import com.min.board.model.Member;
+import com.min.board.paging.Pagination;
+import com.min.board.service.BoardService;
 import com.min.board.service.MemberService;
+import com.min.board.service.PagingService;
 import com.min.board.validator.MemberValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -21,6 +25,12 @@ public class AdminController {
     @Autowired
     private MemberValidator memberValidator;
 
+    @Autowired
+    private BoardService boardService;
+
+    @Autowired
+    private PagingService pagingService;
+
     // 관리자 계정 생성 폼
     @GetMapping("/admin/addAdmin")
     public String addAdminForm(Model model) {
@@ -28,17 +38,33 @@ public class AdminController {
         return "/admin/addAdmin";
     }
 
+    // 게시글 관리 페이지
+    @GetMapping("/admin/boardManage")
+    public String allBoardList(Model model,
+                               @RequestParam(required = false, defaultValue = "1") int page,
+                               @RequestParam(required = false, defaultValue = "1") int range,
+                               String searchText) {
+        Pagination pagination = pagingService.getBoardPagination(page, range, searchText, "list");
+        List<Board> boards = boardService.getBoardList(pagination);
+
+        model.addAttribute("pagination", pagination);
+        model.addAttribute("boardList", boards);
+
+        return "/admin/boardManage";
+    }
+
     // 관리자 계정 생성
     @PostMapping("/admin/join")
-    public String addMember(@Valid Member member, BindingResult bindingResult){ // view의 form->input 의 name과 매핑됨.
+    public String addMember(@Valid Member member, BindingResult bindingResult) { // view의 form->input 의 name과 매핑됨.
         memberValidator.validate(member, bindingResult);
 
-        if(bindingResult.hasErrors()) {
+        if (bindingResult.hasErrors()) {
             return "/admin/addAdmin";
         }
 
         int result = memberService.join(member, "ROLE_ADMIN");
-        if(result > 0) {
+
+        if (result > 0) {
             return "redirect:/admin";
         } else {
             return "/admin/addAdmin";
