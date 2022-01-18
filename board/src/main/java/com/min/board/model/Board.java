2bca42a672699fc15bf08149ae0cb14a078fddfe
wmdwjddyd6 <wmdwjddyd6@naver.com>
2022-01-18 22:12:11 +0900

    [View, DTO] 게시글 작성 및 수정 검증 방식 변경
    자바스크립트로 값이 유효한지 검증

--- board/src/main/java/com/min/board/model/Board.java
+++ board/src/main/java/com/min/board/model/Board.java
@@ -7,21 +7,13 @@ import java.sql.Timestamp;
 
 @Data
 public class Board {
-
     private Long id;
-
-    @NotNull
-    @Size(min = 2, max = 30, message = "제목은 2자 이상 30자 이하입니다.")
     private String title;
-
-    @NotNull
-    @NotBlank(message = "내용을 입력하세요.")
     private String content;
     private Long writerId;
     private String writer;
     private String deleteYN;
     private Timestamp createDate;
     private Long views;
-
     private String type;
 }
