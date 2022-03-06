4c754d716890ddeff174c788f05fb3ca657c84e8
wmdwjddyd6 <wmdwjddyd6@naver.com>
2022-03-06 15:51:42 +0900

    [schema] Mysql schema 업데이트

---
+++ board/src/main/resources/db/mysql/schema.sql
@@ -0,0 +1,54 @@
+CREATE SCHEMA board;
+
+CREATE TABLE `board`.`tb_userinfo` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `role` varchar(20) DEFAULT 'ROLE_USER',
+  `create_date` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`,`username`)
+) ENGINE=InnoDB
+
+CREATE TABLE `board`.`tb_board` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `content` text NOT NULL,
+  `writer_id` int NOT NULL,
+  `writer` varchar(20) NOT NULL,
+  `delete_yn` varchar(1) DEFAULT 'N',
+  `create_date` datetime DEFAULT NULL,
+  `views` int DEFAULT '0',
+  `type` varchar(20) DEFAULT 'board',
+  PRIMARY KEY (`id`),
+  KEY `writer_id` (`writer_id`),
+  CONSTRAINT `tb_board_ibfk_1` FOREIGN KEY (`writer_id`) REFERENCES `tb_userinfo` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB
+
+CREATE TABLE `board`.`tb_comment` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `board_id` int NOT NULL,
+  `content` text NOT NULL,
+  `writer_id` int NOT NULL,
+  `writer` varchar(20) NOT NULL,
+  `create_date` datetime NOT NULL,
+  `type` varchar(20) DEFAULT 'board',
+  PRIMARY KEY (`id`),
+  KEY `board_id` (`board_id`),
+  KEY `writer_id` (`writer_id`),
+  CONSTRAINT `tb_comment_ibfk_1` FOREIGN KEY (`board_id`) REFERENCES `tb_board` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `tb_comment_ibfk_2` FOREIGN KEY (`writer_id`) REFERENCES `tb_userinfo` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB
+
+CREATE TABLE `board`.`tb_file` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `board_id` int NOT NULL,
+  `original_file_name` varchar(100) NOT NULL,
+  `size` int NOT NULL,
+  `path` varchar(100) NOT NULL,
+  `stored_file_name` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `board_id` (`board_id`),
+  CONSTRAINT `tb_file_ibfk_1` FOREIGN KEY (`board_id`) REFERENCES `tb_board` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB
